fa432ff5a222c209e2b545324dd43c4ffbd161cb
2vedant8 <vanshgupta028@gmail.com>
2024-06-23 21:19:18 +0530

    domain chnge socket contect chnge

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-master-eyz2.onrender.com", {
+			const socket = io("chatify.vedantuu.me", {
 				query: {
 					userId: authUser._id,
 				},
