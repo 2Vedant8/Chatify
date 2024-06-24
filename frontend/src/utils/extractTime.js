b5448ea8cdd7d61dc602b9233c0fdb0fb1a18087
2vedant8 <vanshgupta028@gmail.com>
2024-06-25 01:14:42 +0530

    date

--- frontend/src/utils/extractTime.js
+++ frontend/src/utils/extractTime.js
@@ -2,7 +2,7 @@ export function extractTime(dateString) {
 	const date = new Date(dateString);
 	const hours = padZero(date.getHours());
 	const minutes = padZero(date.getMinutes());
-	return `${hours}:${minutes}`;
+	return `${date}:${hours}:${minutes}`;
 }
 
 // Helper function to pad single-digit numbers with a leading zero
